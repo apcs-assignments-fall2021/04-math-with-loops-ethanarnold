4b3435cab8f8f2c85f5076df9d0eac2446941752
ethanarnold <emailtoethan@gmail.com>
2021-09-12 15:49:30 -0700

    made some math methods that calculate factorials, approximate e, and approximate square roots for any number, pos or neg.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,32 +1,66 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class MyMain {
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int res = x;
+        if(x==0) return 1;
+        for(int i = 1; i<x; i++) {
+            res = res*i;
+        }
+        return res;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
+    /*
+    E == 1/x!+1/x+1!...
+     */
+
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int i=0;
+        double res = 0;
+        while(i<20) {
+            res += 1/factorial(i);
+            i++;
+        }
+        return res;
     }
 
     // Calculates the square root iteratively, using the Babylonian method
-    public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+    public static String babylonian(double x) {
+        String res = "";
+        double posX = Math.abs(x);
+        double guess = posX;
+        double nextGuess;
+        double diff = Double.MAX_VALUE;
+        double lastGuess;
+
+        while(Math.abs(diff)>0.00001) {
+            lastGuess = guess;
+            nextGuess = (guess + posX/guess)/2;
+            guess = nextGuess;
+            diff = nextGuess-lastGuess;
+
+        }
+        if(x>=0) {
+            return res + guess;
+        } else {
+            return res + guess + "i";
+        }
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // YOUR CODE HERE
+        System.out.println("give a number");
+        double in = scan.nextDouble();
+        System.out.println("The square root of " + in + " is approximately " + babylonian(in) +".");
+        System.out.println("The value of e is approximately " + calculateE() + ".");
 
 
         
